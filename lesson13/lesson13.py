94201d0d451ea24d6eff84f7996f37d15976aa43
Ron_python <ab20021109@gmail.com>
2024-07-29 07:07:49 +0000

    1

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,14 +1,19 @@
 import requests
 from pprint import pprint
 
-def main():
+def bike_conection():
     youbike_url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/json?size=1000'
     try:
-        response = requests.get(youbike_url)
+        Response = requests.get(youbike_url)
     except Exception  as error:
-        print(error)
+        return error
     else:
-        data = response.json()
+        data = Response.json()
+        return Response
+
+def main():
+    response = isinstance(bike_conection,Response)
+    if response:
         while True:
             district = input("請輸入新北市行政區: ")
             district = district + "區"
@@ -19,10 +24,10 @@ def main():
                     district_stations.append(station)
 
 
-            if district_stations:
-                pprint(district_stations)
-                break
-            else:
-                print(f"沒有找到 {district} 行政區的站點資訊。請再輸入一次")
+                if district_stations:
+                    pprint(district_stations)
+                    break
+                else:
+                    print(f"沒有找到 {district} 行政區的站點資訊。請再輸入一次")
 if __name__== '__main__':
     main()

6c7ed951d0abe0ac39726e7c64fde74867c381db
Ron <ron20021109@gmail.com>
2024-08-08 15:53:27 +0800

    1

--- lesson19/lesson19_2.py
+++ lesson19/lesson19_2.py
@@ -5,5 +5,4 @@ if __name__ == "__main__":
     stations_imfo = get_station_imfo()
     all_in_out = in_out_imformation(name_list,stations_imfo)
     station_in_out = pd.concat(all_in_out)
-    data=station_in_out[station_in_out['日期'].isin(['2020-01-01'])]
-    data.to_csv("當日進出站人數.csv")
+    station_in_out.to_csv('進出站人數表.csv')
